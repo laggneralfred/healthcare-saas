e7167d358127b2f7b46e06d81473280959c0f553
Alfred Laggner <laggner.alfred@gmail.com>
2026-03-30 04:29:30 -0700

    describe what you changed

--- app/Filament/Pages/Settings/ImportPatients.php
+++ app/Filament/Pages/Settings/ImportPatients.php
@@ -8,7 +8,7 @@ use App\Services\CSVImportService;
 use App\Services\PracticeContext;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
