cd1641bf453a98fb73196262ce8cf4b434e13e0a
Alfred Laggner <laggner.alfred@gmail.com>
2026-04-04 19:14:29 -0700

    Fix: add symfony/http-client and mailgun entry to services.php

--- config/services.php
+++ config/services.php
@@ -49,4 +49,11 @@ return [
         ],
     ],
 
+'mailgun' => [
+    'domain' => env('MAILGUN_DOMAIN'),
+    'secret' => env('MAILGUN_SECRET'),
+    'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    'scheme' => 'https',
+],
+
 ];
